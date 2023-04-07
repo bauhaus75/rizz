dac74e7dee7eefa1d6f11ff072e71683f0a2047a
bauhaus75 <63088327+bauhaus75@users.noreply.github.com>
2023-04-07 13:37:36 -0400

    Update ella_rizz.cpp

--- ella_rizz.cpp
+++ ella_rizz.cpp
@@ -4,11 +4,17 @@
 using namespace std;
 
 int main() {
-    string reply_ella;
+    string reply_ella = "";
     while (reply_ella.find("PLEASE TAKE MY NUMBER!! MY NUMBER IS ") == string::npos) {
-        cout << "wow we love a woman in stem" << endl << "PLEASE ELLA PLEASE RESPOND (with rizz)" << endl;
-        getline(cin, reply_ella);
-        cout << endl;
+        if (reply_ella == " ") {
+            cout << "wow we love a woman in stem" << endl << "PLEASE ELLA PLEASE RESPOND (with rizz)" << endl;
+            getline(cin, reply_ella);
+            cout << endl;
+        } else {
+            cout << "i literally cannot comprehend or accept that as a response" << endl << "PLEASE ELLA PLEASE RESPOND (with rizz)" << endl;
+            getline(cin, reply_ella);
+            cout << endl;    
+        }
     }
     cout << "yay" << endl;
     return 0;
