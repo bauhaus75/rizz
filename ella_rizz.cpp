26fa5927a657c01d3df0872232c2ace4e5145400
bauhaus75 <63088327+bauhaus75@users.noreply.github.com>
2023-04-06 21:09:56 -0400

    Add files via upload

---
+++ ella_rizz.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+	string reply_ella;
+	while (!reply_ella.find("PLEASE TAKE MY NUMBER!! MY NUMBER IS ")) {
+		cout << "wow we love a woman in stem" << endl << "PLEASE ELLA PLEASE RESPOND (with rizz)" << endl;
+		cin >> reply_ella;
+		cout << endl;
+	}
+	cout << "yay" << endl;
+	return 0;
+}
