a1f91745e24253a801d05a46bec5cef5e0cd093c
bauhaus75 <63088327+bauhaus75@users.noreply.github.com>
2023-04-06 22:02:23 -0400

    Update ella_rizz.cpp

--- ella_rizz.cpp
+++ ella_rizz.cpp
@@ -3,13 +3,13 @@
 
 using namespace std;
 
-int main () {
-	string reply_ella;
-	while (!reply_ella.find("PLEASE TAKE MY NUMBER!! MY NUMBER IS ")) {
-		cout << "wow we love a woman in stem" << endl << "PLEASE ELLA PLEASE RESPOND (with rizz)" << endl;
-		cin >> reply_ella;
-		cout << endl;
-	}
-	cout << "yay" << endl;
-	return 0;
+int main() {
+    string reply_ella;
+    while (reply_ella.find("PLEASE TAKE MY NUMBER!! MY NUMBER IS ") == string::npos) {
+        cout << "wow we love a woman in stem" << endl << "PLEASE ELLA PLEASE RESPOND (with rizz)" << endl;
+        cin >> reply_ella;
+        cout << endl;
+    }
+    cout << "yay" << endl;
+    return 0;
 }
