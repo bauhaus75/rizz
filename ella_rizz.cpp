e3229fa3f052f0fbc72298e58be32e955169ac58
bauhaus75 <63088327+bauhaus75@users.noreply.github.com>
2023-04-07 13:32:27 -0400

    Update ella_rizz.cpp

--- ella_rizz.cpp
+++ ella_rizz.cpp
@@ -7,7 +7,7 @@ int main() {
     string reply_ella;
     while (reply_ella.find("PLEASE TAKE MY NUMBER!! MY NUMBER IS ") == string::npos) {
         cout << "wow we love a woman in stem" << endl << "PLEASE ELLA PLEASE RESPOND (with rizz)" << endl;
-        cin >> reply_ella;
+        getline(cin, reply_ella);
         cout << endl;
     }
     cout << "yay" << endl;
